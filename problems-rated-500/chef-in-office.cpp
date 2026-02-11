c5fef68db7afaf18bd30f37cf04763402193e718
brsrk94 <brsrk94@proton.me>
2026-02-11 23:41:04 +0530

    day5 problem solved

---
+++ problems-rated-500/chef-in-office.cpp
@@ -0,0 +1,16 @@
+// Chef in Office : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int X,Y;
+	    cin >> X >> Y;
+	    cout << X * 4 + Y << endl;
+	}
+
+}
