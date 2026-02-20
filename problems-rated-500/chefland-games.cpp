a5a7cc24dc6b01728b7309a5acc5f0278155d4fb
brsrk94 <brsrk94@proton.me>
2026-02-20 23:01:43 +0530

    chefland-games

---
+++ problems-rated-500/chefland-games.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int R1,R2,R3,R4 = 0;
+	    cin >> R1 >> R2 >> R3 >> R4;
+	    // 0 - INSIDE LIMITS , 1 - OUTSIDE LIMITS
+
+
+	    
+	    if ( R1 == 0 && R2 == 0 && R3 == 0 && R4 == 0){
+	        cout << "IN" << endl;
+	    }else {
+	        cout << "OUT" << endl;
+	    }
+	}
+
+}
