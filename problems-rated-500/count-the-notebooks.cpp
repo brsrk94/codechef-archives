b5fc5442430c6c0066c9c0fcdad39e19a8a10270
brsrk94 <brsrk94@proton.me>
2026-02-26 21:27:13 +0530

    count-the-notebooks

---
+++ problems-rated-500/count-the-notebooks.cpp
@@ -0,0 +1,19 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int N;
+	    cin >> N;
+	    int total = N * 1000 / 100;
+	    cout << total << endl;
+	    
+	    
+	}
+
+}
